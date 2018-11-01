e5091062df31079a95fd8e3fd9a08e743fb98fdc
timkittel <19408882+timkittel@users.noreply.github.com>
2018-11-01 12:57:50 +0100

    updated the docs (#6)

--- src/operationpoint.jl
+++ src/operationpoint.jl
@@ -4,12 +4,30 @@
 using PowerDynBase: AbstractState, GridDynamics, State, GridDynamics
 using NLsolve
 
+"""
+    struct RootFunction
+        grid::GridDynamics
+    end
+
+Basic data structure providing a method to evalute a subtype of [`PowerDynSolve.GridDynamics`](@ref) as
+a right-hand-side function that can be used for root searching.
+"""
 struct RootFunction
     grid::GridDynamics
 end
+"""
+    function (r::RootFunction)(x_out, x_in)
+
+Evaluate the power grid dynamics of `r` for `x_in` and write it in `x_out`.
+"""
 function (r::RootFunction)(x_out, x_in)
     r.grid(x_out, x_in, 0., 0.)
 end
+"""
+    function (r::RootFunction)(x_in)
+
+Evaluate the power grid dynamics of `r` for `x_in` and return the result.
+"""
 function (r::RootFunction)(x_in::AbstractVector)
     x_out = similar(x_in)
     r.grid(x_out, x_in, 0., 0.)
@@ -19,6 +37,14 @@ function (r::RootFunction)(s::AbstractState{G, V, T}) where {G, V, T}
     r(convert(Vector{V}, s))
 end
 
+"""
+    function operationpoint(start::AbstractState)
+    function operationpoint(grid::GridDynamics, start::AbstractState)
+    function operationpoint(grid::GridDynamics, start::AbstractVector)
+
+Find the operation point (fixed point of the right-hand-side) of a power grid (either `grid` or inferred)
+by using the initial value `start`.
+"""
 function operationpoint(start::AbstractState{G, V, T}) where {G, V, T}
     if SlackAlgebraic ∉ start |> Nodes .|> parametersof .|> typeof
         @warn "currently not making any checks concerning assumptions
@@ -36,12 +62,10 @@ function operationpoint(start::AbstractState{G, V, T}) where {G, V, T}
     end
     State(grid, res.zero)
 end
-
 function operationpoint(grid::GridDynamics, start::AbstractState)
     @assert grid === GridDynamics(start)
     operationpoint(start)
 end
-
 function operationpoint(grid::GridDynamics, start::AbstractVector)
     operationpoint(State(grid, start))
 end
--- src/solve.jl
+++ src/solve.jl
@@ -5,6 +5,14 @@ using PowerDynBase: AbstractState, OrdinaryGridDynamics, OrdinaryGridDynamicsWit
 
 import DiffEqBase: solve
 
+"""
+    function solve(p::GridProblem; kwargs...)
+
+Solve a [`PowerDynSolve.GridProblem`](@ref), using DifferentialEquations.jl in the back.
+The correct solvers are automatically chosen.
+
+`kwargs` are the keyword arguments that are simply passed to DifferentialEquations.jl.
+"""
 function solve(p::GridProblem{P, <:AbstractState{OrdinaryGridDynamics, V, T}, Q}; kwargs...) where {P, V, T, Q}
     GridSolution(solve(p.prob, Rodas4(); kwargs...), GridDynamics(p.start))
 end
@@ -18,6 +26,12 @@ function solve(p::GridProblem{P, <:AbstractState{G, V, T}, Q}; kwargs...) where 
     # GridSolution(solve(p.prob, Rodas4(); kwargs...), GridDynamics(p.start))
 end
 
+"""
+    function solve(g::GridDynamics, x0, timespan)
+
+Solve a power grid `g` (of type [`PowerDynSolve.GridDynamics`](@ref)) starting at `x0` for a `timespan`, using DifferentialEquations.jl in the back.
+The correct solvers are automatically chosen.
+"""
 function solve(g::GridDynamics, x0, timespan)
     solve(GridProblem(g, x0, timespan))
 end
