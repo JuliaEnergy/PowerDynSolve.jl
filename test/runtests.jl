89f50cbba803403051c6647c58f3d8dfda266d3f
ciem <tim.kittel@elena-international.com>
2019-01-18 08:07:53 +0100

    removed op stuff and added op package

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,6 @@
 using Test
 
 testlist = [
-    ("rootfunction.jl", "Root Function Tests"),
-    ("operationpoint.jl", "Operation Point Tests"),
     ("intergration.jl", "Integration Tests"),
     ("gridsolutions.jl", "Grid Solutions Tests"),
     ("plotrecipes.jl", "Plot Recipes Tests"),
